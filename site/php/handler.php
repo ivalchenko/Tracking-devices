f554471737ef43e5d75d1dc07487db121cbfae7e
ivalchenko <i.valchenk@hotmail.com>
2015-04-20 21:55:50 +0300

    Modify handler php script

--- site/php/handler.php
+++ site/php/handler.php
@@ -1,8 +1,13 @@
 <?php
 	include 'db_connector.php';
 
+	//$send = $_POST['button'];
+	//$date = $_POST['calendar'];
+	$date = "2015-04-20";
+
 	$startFrom = $_POST['startFrom'];
-	$res = mysqli_query($db, "SELECT * FROM `coordinates` ORDER BY `id` DESC LIMIT {$startFrom}, 4");
+	$res = mysqli_query($db, "SELECT * FROM `coordinates` ORDER BY `id` DESC LIMIT {$startFrom}, 1"); 
+	//$res = mysqli_query($db, "SELECT * FROM `coordinates` WHERE `date` BETWEEN '$date 00:00:00' AND '$date 23:59:59' LIMIT {$startFrom}, 1"); 
 
 	$articles = array();
 	while ($row = mysqli_fetch_assoc($res))
