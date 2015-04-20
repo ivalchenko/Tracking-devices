f952040745665e626c7c4d28e26fde7b7f47572d
ivalchenko <i.valchenk@hotmail.com>
2015-04-20 22:00:08 +0300

    Modify AJAX query

--- site/js/ajax/coordinates_load.js
+++ site/js/ajax/coordinates_load.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
 
-alert("Документ удачно загружен");
-
 /* Переменная-флаг для отслеживания того, происходит ли в данный момент ajax-запрос. В самом начале даем ей значение false, т.е. запрос не в процессе выполнения */    
 var inProgress = false;
 /* С какой статьи надо делать выборку из базы при ajax-запросе */ 
 var startFrom = 1;
+var counter = 1;
 
+ymaps.ready(init);
 var myMap, myPlacemark;
 
-myMap = new ymaps.Map("map", {
-    center: [53.8912, 27.5669],
-    zoom: 10
-});
+ function init(){     
+    myMap = new ymaps.Map("map", {
+        center: [53.8912, 27.5669],
+        zoom: 18
+    });
+}
 
-alert("Карта добавлена");
-        
+function ajax_function(){
         $.ajax({            
             /* адрес файла-обработчика запроса */
-            url: 'php/handler.php',
+            url: 'php/handler.php', //
             /* метод отправки данных */
             method: 'POST',
             /* данные, которые мы передаем в файл-обработчик */
@@ -30,37 +31,36 @@ alert("Карта добавлена");
             /* что нужно сделать по факту выполнения запроса */            
         }).done(function(data){
 
-            alert("Подключились к PHP файлу");
-
             /* Преобразуем результат, пришедший от обработчика - преобразуем json-строку обратно в массив */ 
             data = jQuery.parseJSON(data);
 
-            alert("Прорарсили json-строку в массив");
 
             /* Если массив не пуст (т.е. статьи там есть) */
             if (data.length > 0) {
-
-                 alert("Длина масива больше нуля");
 
             /* Делаем проход по каждому результату, оказвашемуся в массиве,
             где в index попадает индекс текущего элемента массива, а в data - сама статья */                 
             $.each(data, function(index, data){
 
-                alert("Готовимся добавить точку");
-
                 myPlacemark = new ymaps.Placemark([data.lat, data.lon], { 
-                    content: 'Current location', 
-                    balloonContent: 'Your device' 
+                    content: counter, 
+                    balloonContent: data.time
+                    //balloonContent: '<img src="https://d3da265we6b140.cloudfront.net/tasks/assets/img/marketing/examples/mcenroe-after-500px.png">'
                 });
 
+                myMap.setCenter([data.lat, data.lon]);
+
                 myMap.geoObjects.add(myPlacemark);
-                alert("Добавили точку на карту");
+                counter++;
             });
                     
            
             /* По факту окончания запроса снова меняем значение флага на false */
             inProgress = false;
             // Увеличиваем на 10 порядковый номер статьи, с которой надо начинать выборку из базы
-            // startFrom += 18;
-        }});   
+            startFrom += 1;
+        }});
+    } // end of ajax wrapper function
+
+    setInterval(ajax_function, 4000);
 });
