be916358fa1c5277e5a126df35c46b63db0abdd8
ivalchenko <i.valchenk@hotmail.com>
2015-04-23 06:43:59 +0300

    Modify js control

--- site/js/ajax/coordinates_load.js
+++ site/js/ajax/coordinates_load.js
@@ -1,66 +1,124 @@
 $(document).ready(function(){
+    
+    var inProgress = false;
+    var startFrom = 1; // С какой статьи надо делать выборку из базы при ajax-запросе
+    var counter = 1; // Убрать
+    var isLive = false;
 
-/* Переменная-флаг для отслеживания того, происходит ли в данный момент ajax-запрос. В самом начале даем ей значение false, т.е. запрос не в процессе выполнения */    
-var inProgress = false;
-/* С какой статьи надо делать выборку из базы при ajax-запросе */ 
-var startFrom = 1;
-var counter = 1;
+    /*ymaps.ready(init);
+    var myMap, myPlacemark;
+    var myCollection;  */
 
-ymaps.ready(init);
-var myMap, myPlacemark;
+     function init(){  
+        myCollection = new ymaps.GeoObjectCollection();
 
- function init(){     
-    myMap = new ymaps.Map("map", {
-        center: [53.8912, 27.5669],
-        zoom: 18
-    });
-}
+        myMap = new ymaps.Map("map", {
+            center: [53.8912, 27.5669],
+            zoom: 18
+        });
+    }
 
-function ajax_function(){
+    window.module = {
+      trackByDate: function () {
+        alert('Track by DATE');
+        date = document.forms["track-by-date"].elements["track-by-date"].value;
+        myCollection.removeAll(); // удаляем прошлые метки
+        ajax_past_function(date);
+      },
+
+      startLiveTrack: function () {
+        alert('Live tracking: ON');
+        isLive = true;
+        myCollection.removeAll();
+        setInterval(ajax_live_function, 4000);
+      },
+
+      stopLiveTrack: function () {
+        alert('STOP tracking!');
+        isLive = false;
+      }
+    }
+
+    function ajax_live_function(){
+
+        if (isLive) 
+        {
+            $.ajax({            
+                url: 'php/handler.php', 
+                method: 'POST',
+                data: {"startFrom" : startFrom},
+
+                beforeSend: function() {
+                    inProgress = true; // меняем значение флага на true, т.е. запрос сейчас в процессе выполнения 
+                }
+                // что нужно сделать по факту выполнения запроса           
+            }).done(function(data){
+                // Преобразуем результат, пришедший от обработчика - преобразуем json-строку обратно в массив 
+                data = jQuery.parseJSON(data);
+
+                // Если массив не пуст (т.е. статьи там есть) 
+                if (data.length > 0) {
+
+                /* Делаем проход по каждому результату, оказвашемуся в массиве,
+                где в index попадает индекс текущего элемента массива, а в data - сама статья */                 
+                $.each(data, function(index, data){
+
+                    myPlacemark = new ymaps.Placemark([data.lat, data.lon], { 
+                        content: counter, 
+                        balloonContent: data.time
+                    });
+
+                    myMap.setCenter([data.lat, data.lon]); // центрируем карту на новом положении отслеиваемого объекта
+                    myCollection.add(myPlacemark); 
+
+                    //myMap.geoObjects.add(myPlacemark);
+                    myMap.geoObjects.add(myCollection);
+                    counter++;
+                });
+                        
+                // По факту окончания запроса снова меняем значение флага на false 
+                inProgress = false;
+                startFrom += 1;
+            }});
+        }
+    } 
+
+    function ajax_past_function(date){
         $.ajax({            
-            /* адрес файла-обработчика запроса */
-            url: 'php/handler.php', //
-            /* метод отправки данных */
+            url: 'php/handler_by_date.php',
             method: 'POST',
-            /* данные, которые мы передаем в файл-обработчик */
-            data: {"startFrom" : startFrom},
-            /* что нужно сделать до отправки запрса */
+            data: {"startFrom" : startFrom, "date" : date},
+  
             beforeSend: function() {
-                inProgress = true; /* меняем значение флага на true, т.е. запрос сейчас в процессе выполнения */
+                inProgress = true; 
             }
-            /* что нужно сделать по факту выполнения запроса */            
+           
         }).done(function(data){
-
-            /* Преобразуем результат, пришедший от обработчика - преобразуем json-строку обратно в массив */ 
+ 
             data = jQuery.parseJSON(data);
 
-
-            /* Если массив не пуст (т.е. статьи там есть) */
             if (data.length > 0) {
+                $.each(data, function(index, data){
 
-            /* Делаем проход по каждому результату, оказвашемуся в массиве,
-            где в index попадает индекс текущего элемента массива, а в data - сама статья */                 
-            $.each(data, function(index, data){
+                    myPlacemark = new ymaps.Placemark([data.lat, data.lon], { 
+                        content: counter, 
+                        balloonContent: data.time
+                        //balloonContent: '<img src="https://d3da265we6b140.cloudfront.net/tasks/assets/img/marketing/examples/mcenroe-after-500px.png">'
+                    });
 
-                myPlacemark = new ymaps.Placemark([data.lat, data.lon], { 
-                    content: counter, 
-                    balloonContent: data.time
-                    //balloonContent: '<img src="https://d3da265we6b140.cloudfront.net/tasks/assets/img/marketing/examples/mcenroe-after-500px.png">'
+                    myMap.setCenter([data.lat, data.lon]);
+                    myCollection.add(myPlacemark); 
+
+                    //myMap.geoObjects.add(myPlacemark);
+                    myMap.geoObjects.add(myCollection);
+                    counter++;
                 });
-
-                myMap.setCenter([data.lat, data.lon]);
-
-                myMap.geoObjects.add(myPlacemark);
-                counter++;
-            });
                     
-           
-            /* По факту окончания запроса снова меняем значение флага на false */
-            inProgress = false;
-            // Увеличиваем на 10 порядковый номер статьи, с которой надо начинать выборку из базы
-            startFrom += 1;
+                inProgress = false;
+                startFrom += 1;
         }});
-    } // end of ajax wrapper function
+    } 
 
-    setInterval(ajax_function, 4000);
 });
+
+
